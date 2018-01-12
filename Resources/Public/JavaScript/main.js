3063ad9bd01cb4c45fae1e91fbe53a9c6205442c
Nerijus Noreika <Akimbas123@gmail.com>
2018-01-12 23:10:43 +0200

    Add couple of lines of javascript

---
+++ Resources/Public/JavaScript/main.js
@@ -0,0 +1,30 @@
+// init a 'global closure'
+
+(function() {
+    let container = document.querySelector('.tx_enenos_slider');
+    
+    
+    let properties = {
+        duration: 5,
+        type: ''
+    };
+    // Let's start this party if its here ;) 
+    if (container !== 'undefined'){
+        let slides = document.querySelector('.tx_enenos_slider .slides');
+        if (!checkCount(slides)) 
+            return;
+        
+
+        
+    } else {
+        return; 
+    }
+    
+
+
+    function checkCount(slides) {
+        if (slides.children.length > 10) 
+            return false;
+        
+    }
+})();
